a5ece3f941f085e030f6fe48e2f33b2db5afa579
Caio Ortu <caio.ortu@cornershopapp.com>
2021-07-27 18:31:00 -0300

    Adding `FeedErrorView` and `FeedErrorViewModel` to `FeedPresenter`

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedErrorViewModel.swift
+//  MVP
+//
+//  Created by Caio Ortu on 7/27/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+struct FeedErrorViewModel {
+	let message: String
+}
